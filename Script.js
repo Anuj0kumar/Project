d742c2c5d2a672b3b0c8ea8412309d26604831c8
Anuj0kumar <158174480+Anuj0kumar@users.noreply.github.com>
2024-09-03 01:27:03 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+// script.js
+// Add event listener to nav links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const sectionId = link.getAttribute('href');
+        const section = document.querySelector(sectionId);
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
